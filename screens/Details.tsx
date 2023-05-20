4a0cd8e1c9fb7f19745bda8a7f228a4887cbc1ab
vlaier <piotrzielu.1996@gmail.com>
2023-05-20 19:11:34 +0200

    feat: add Details screen component

---
+++ screens/Details.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Details = () => {
+  return (
+    <View>
+      <Text>Comic Book Details</Text>
+    </View>
+  );
+};
+export default Details;
