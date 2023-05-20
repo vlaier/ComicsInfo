f1cf0b7c9485024a921d543d06860cefa3868941
vlaier <piotrzielu.1996@gmail.com>
2023-05-20 19:07:22 +0200

    feat: add RootParamList interface to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { ParamListBase } from '@react-navigation/routers';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import Home from './screens/Home';
+import Details from './screens/Details';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
+}
+export interface RootParamList extends ParamListBase {
+  Home: undefined;
+  Details: undefined;
 }
