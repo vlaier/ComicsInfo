a520b9a4131cb8f5d23db18edf43cbab99346425
vlaier <piotrzielu.1996@gmail.com>
2023-05-21 07:53:21 +0200

    refactor: change data structure

--- screens/Details.tsx
+++ screens/Details.tsx
@@ -5,7 +5,11 @@ import { RootParamList } from '../App';
 
 const Details = ({ route }: StackScreenProps<RootParamList, 'Details'>) => {
   const comic = route.params;
-
+  const date = new Date(
+    parseInt(comic.year),
+    parseInt(comic.month),
+    parseInt(comic.day)
+  );
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <View style={styles.imageBox}>
@@ -16,24 +20,11 @@ const Details = ({ route }: StackScreenProps<RootParamList, 'Details'>) => {
           resizeMode="contain"
         />
       </View>
-      <View style={styles.imageBox}>
-        <Image
-          style={{ flex: 1 }}
-          source={{ uri: comic.img }}
-          alt={comic.alt}
-          resizeMode="contain"
-        />
+      <View style={styles.infoBox}>
+        <Text style={styles.title}>{comic.title}</Text>
+        <Text style={styles.date}>{date.toDateString()}</Text>
+        <Text>{comic.alt}</Text>
       </View>
-      <View style={styles.imageBox}>
-        <Image
-          style={{ flex: 1 }}
-          source={{ uri: comic.img }}
-          alt={comic.alt}
-          resizeMode="contain"
-        />
-      </View>
-      <Text>{comic.title}</Text>
-      <Text>{JSON.stringify(route.params)}</Text>
     </ScrollView>
   );
 };
@@ -47,6 +38,17 @@ const styles = StyleSheet.create({
     alignContent: 'stretch',
     height: 300,
     paddingVertical: 20,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  date: {
+    fontSize: 12,
+    color: '#888',
+  },
+  infoBox: {
+    paddingHorizontal: 10,
   },
 });
 export default Details;
