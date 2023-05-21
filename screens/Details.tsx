3b141af996fd4cc50c8c1df0f56c43854028a79a
vlaier <piotrzielu.1996@gmail.com>
2023-05-21 03:23:08 +0200

    feat: display content in Details screen

--- screens/Details.tsx
+++ screens/Details.tsx
@@ -1,11 +1,52 @@
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { RootParamList } from '../App';
 
-const Details = () => {
+const Details = ({ route }: StackScreenProps<RootParamList, 'Details'>) => {
+  const comic = route.params;
+
   return (
-    <View>
-      <Text>Comic Book Details</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={styles.imageBox}>
+        <Image
+          style={{ flex: 1 }}
+          source={{ uri: comic.img }}
+          alt={comic.alt}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.imageBox}>
+        <Image
+          style={{ flex: 1 }}
+          source={{ uri: comic.img }}
+          alt={comic.alt}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.imageBox}>
+        <Image
+          style={{ flex: 1 }}
+          source={{ uri: comic.img }}
+          alt={comic.alt}
+          resizeMode="contain"
+        />
+      </View>
+      <Text>{comic.title}</Text>
+      <Text>{JSON.stringify(route.params)}</Text>
+    </ScrollView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+  },
+  imageBox: {
+    alignContent: 'stretch',
+    height: 300,
+    paddingVertical: 20,
+  },
+});
 export default Details;
